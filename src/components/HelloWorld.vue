1642885e74602317906e64c7eed7e9f5df1d5728
jason <13141374484@163.com>
2019-01-14 21:31:19 +0800

    首页 页面

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,41 @@
 <template>
   <div class="star-index">
+    <div class="png-wrapper"></div>
+    <div class="star-index-top">
+      <div class="star-index-top-asset">
+          <img src="../assets/资产.svg" alt="">
+          <span>资产：1536896</span>
+        </div>
+      <div class="star-index-top-power">
+           <img src="../assets/Shape.svg" alt="">
+        <span>当前星力</span>
+      </div>
 
+    </div>
+    <div class="star-index-body">
+        <div class="ball1"><span >1</span></div>
+        <div class="ball2"><span >1</span></div>
+        <div class="ball3"><span ></span></div>
+        <div class="ball4"><span ></span></div>
+        <div class="ball5"><span ></span></div>
+        <div class="ball6"><span ></span></div>
+        <div class="ball7"><span ></span></div>
+    </div>
+    <div class="star-index-footer">
+      <div class="star-index-footer-invite">
+        <div class="wrapper">
+          <img src="../assets/邀请.svg" alt="">
+          邀请好友
+        </div>
+      </div>
+      <div class="star-index-footer-wallet">
+        <div class="wrapper">
+          <img src="../assets/Shape1.svg" alt="">
+          星链钱包
+        </div>
+
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,10 +50,94 @@ export default {
 
 <style scoped lang="scss">
   .star-index{
-    border: 1px solid red;
     height: 100vh;
-    width: 100vh;
-    background: url("../assets/index.png") center center no-repeat fixed;
+    z-index: 1;
+    .png-wrapper{
+      background: url("../assets/index.png")  center center no-repeat ;
+      background-size: cover;
+      position:fixed;
+      top: 0;
+      left: 0;
+      width:100%;
+      height:100%;
+      z-index: -10;
+    }
+    &-top{
+      height: 5vh;
+      color: #fff;display: flex;align-items: center;margin-top: 3.3em;
+      &-asset{margin-left: 1.45em;display: flex;align-items: center;
+        >img{width: 2vh} }
+      &-power{margin-left: 1.45em;display: flex;align-items: center; >img{width: 2vh}
+      }
+    }
+    &-body{
+      margin-top: 20vh;
+      height: 30vh;
+      div{width: 0.95em;
+        height: 0.95em;
+        border-radius: 50%;
+        border: 1px solid #fff;}
+      .ball1{border-color: #CB18FF;
+        span{
+          padding: .2em;
+          width: .4em;
+          height: .4em;
+          border-radius: 50%;
+        background: #cb18ff;}
+      }
+      .ball2{border-color: #058EFD;}
+      .ball3{border-color: #fff;}
+      .ball4{border-color: #058B1F;}
+      .ball5{border-color: #CB18FF;}
+      .ball6{border-color: #FF1E60;}
+      .ball7{border-color: #F3FF4C
+      ;}
+    }
+    &-footer{
+      color: #fff;
+      margin-top: 20vh;
+      display: flex;
+      align-items: center;
+      &-invite{
+        margin-right: 4vh;
+        width: 25vh;
+        .wrapper{
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          padding-top: 1vh;
+          padding-bottom: 1vh;
+        }
+        img{width: 17pt;}
+
+        background: url("../assets/Background Copy 2.svg") center no-repeat ;
+        background-size: cover;
+        font-size: 14pt;
+        margin-left: 1.8em;
+      }
+      &-wallet{
+        width: 25vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        .wrapper{
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          padding-top: 1vh;
+          padding-bottom: 1vh;
+        }
+        img{width: 3vh;}
+        background: url("../assets/Background Copy 2.svg") center no-repeat ;
+        background-size: cover;
+        font-size: 14pt;
+        margin-right: 1.8em;
+
+
+      }
+
+    }
   }
 </style>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld />
   </div>
 </template>
 
@@ -16,3 +15,9 @@ export default {
   }
 }
 </script>
+<style>
+  .home{
+    border: 1px solid;
+    height: 100vh;
+  }
+</style>
